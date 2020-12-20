ce327ff03dd2763c621b211e6aadcec5969a94f6
otherPOM <otherPOM@gmail.com>
2020-12-20 13:12:57 +0300

    Stage 1 done

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("X O X\n" +
+                "O X O\n" +
+                "X X O ");
     }
 }
