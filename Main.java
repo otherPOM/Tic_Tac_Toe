fae97bae0b68d58c70a9c457ce9e5570367c47ea
otherPOM <otherPOM@gmail.com>
2020-12-20 13:21:13 +0300

    Stage 2 done

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("X O X\n" +
-                "O X O\n" +
-                "X X O ");
+        System.out.print("Enter cells: ");
+        var cells = scan.nextLine();
+        printGrid(cells);
+    }
+
+    private static void printGrid(String cells) {
+        System.out.println("---------");
+        for (int i = 0; i < 9; i += 3) {
+            System.out.println("| " + cells.charAt(i) +
+                    " " + cells.charAt(i + 1) +
+                    " " + cells.charAt(i + 2) + " |");
+        }
+        System.out.println("---------");
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ## Stage 1
 Your first task in this project is to print the game grid in the console output. Use what you’ve learned about the `print()` function to print three lines, each containing three characters (X’s and O’s) to represent a game of tic-tac-toe where all fields of the grid have been filled in.
+
+## Stage 2
+In this stage, you will write a program that:
+
+1. Reads a string of 9 symbols from the input and displays them to the user in a 3x3 grid. The grid can contain only `X`, `O` and `_` symbols.
+2. Outputs a line of dashes `---------` above and below the grid, adds a pipe `|` symbol to the beginning and end of each line of the grid, and adds a space between all characters in the grid.
